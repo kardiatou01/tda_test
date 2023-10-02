cf755c8b013fec5b2b32c2da84b0930cf5e5606d
kardiatou01 <kardiatoutoure01@gmail.com>
2023-10-02 16:15:13 +0200

    validation deuxieme test

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,16 +1,23 @@
 class FizzBuzz:
-    def affiche(self, nombre):
-        resultat=""
+    @staticmethod
+    def affiche(n1, n2):
+        resultat = ""
 
-        for i in range(1, nombre +1):
-            if i%15==0 :
+        for i in range(n1, n2 + 1):
+            if i % 15 == 0:
                 resultat += "FrisBee"
-            elif i%5==0:
+            elif i % 5 == 0:
                 resultat += "Buzz"
-            elif i%3==0:
+            elif i % 3 == 0:
                 resultat += "Fizz"
             else:
                 resultat += str(i)
 
         return resultat
-        
+
+
+# Appel de la méthode affiche avec deux nombres en entrée
+FizzBuzz.affiche(5, 10)  # Affiche "BuzzFizz78FizzBuzz"
+FizzBuzz.affiche(10, 16) # Affiche "Buzz11Fizz1314FrisBee16"    
+
+    
--- Test_FizzBuzz.py
+++ Test_FizzBuzz.py
@@ -7,8 +7,10 @@ class Test_FizzBuzz(unittest.TestCase):
         self.instance=FizzBuzz()
     
     def test_affiche(self):
-        self.assertEqual(self.instance.affiche(100), "12Fizz4BuzzFizz78FizzBuzz11Fizz1314FrisBee1617Fizz19BuzzFizz2223FizzBuzz26Fizz2829FrisBee3132Fizz34BuzzFizz3738FizzBuzz41Fizz4344FrisBee4647Fizz49BuzzFizz5253FizzBuzz56Fizz5859FrisBee6162Fizz64BuzzFizz6768FizzBuzz71Fizz7374FrisBee7677Fizz79BuzzFizz8283FizzBuzz86Fizz8889FrisBee9192Fizz94BuzzFizz9798FizzBuzz")
-        self.assertEqual(self.instance.affiche(5), "12Fizz4Buzz")
+        self.assertEqual(self.instance.affiche(1, 100), "12Fizz4BuzzFizz78FizzBuzz11Fizz1314FrisBee1617Fizz19BuzzFizz2223FizzBuzz26Fizz2829FrisBee3132Fizz34BuzzFizz3738FizzBuzz41Fizz4344FrisBee4647Fizz49BuzzFizz5253FizzBuzz56Fizz5859FrisBee6162Fizz64BuzzFizz6768FizzBuzz71Fizz7374FrisBee7677Fizz79BuzzFizz8283FizzBuzz86Fizz8889FrisBee9192Fizz94BuzzFizz9798FizzBuzz")
+        self.assertEqual(self.instance.affiche(5, 10), "BuzzFizz78FizzBuzz")
+        self.assertEqual(self.instance.affiche(10, 16), "Buzz11Fizz1314FrisBee16")
+
 
 if __name__=='__main__':
     unittest.main()

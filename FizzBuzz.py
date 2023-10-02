522fa8cd1276d1a3723ae68eae53d01d47837477
kardiatou01 <kardiatoutoure01@gmail.com>
2023-10-02 15:57:08 +0200

    validation premier tests

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,2 +1,16 @@
-import unittest
-from FizzBuzz import *
+class FizzBuzz:
+    def affiche(self, nombre):
+        resultat=""
+
+        for i in range(1, nombre +1):
+            if i%15==0 :
+                resultat += "FrisBee"
+            elif i%5==0:
+                resultat += "Buzz"
+            elif i%3==0:
+                resultat += "Fizz"
+            else:
+                resultat += str(i)
+
+        return resultat
+        
---
+++ Test_FizzBuzz.py
@@ -0,0 +1,14 @@
+import unittest
+from FizzBuzz import *
+
+class Test_FizzBuzz(unittest.TestCase):
+
+    def setUp(self):
+        self.instance=FizzBuzz()
+    
+    def test_affiche(self):
+        self.assertEqual(self.instance.affiche(100), "12Fizz4BuzzFizz78FizzBuzz11Fizz1314FrisBee1617Fizz19BuzzFizz2223FizzBuzz26Fizz2829FrisBee3132Fizz34BuzzFizz3738FizzBuzz41Fizz4344FrisBee4647Fizz49BuzzFizz5253FizzBuzz56Fizz5859FrisBee6162Fizz64BuzzFizz6768FizzBuzz71Fizz7374FrisBee7677Fizz79BuzzFizz8283FizzBuzz86Fizz8889FrisBee9192Fizz94BuzzFizz9798FizzBuzz")
+        self.assertEqual(self.instance.affiche(5), "12Fizz4Buzz")
+
+if __name__=='__main__':
+    unittest.main()
